5aaed3ef4de976cc1d4012cdf8d5108b0d00f0e2
Pierre Fenoll <pierrefenoll@gmail.com>
2024-01-17 16:09:06 +0100

    start
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

---
+++ scrolls/src/ndjson.rs
@@ -0,0 +1,133 @@
+use std::{env, iter::repeat};
+
+use anyhow::Result;
+use libremarkable::appctx::ApplicationContext;
+use log::info;
+use pb::proto::hypercards::{drawing, Drawing};
+use ringbuffer::{AllocRingBuffer, RingBuffer};
+use serde::Deserialize;
+use tokio::time::sleep;
+
+use crate::paint::{paint, DRAWING_PACE, INTER_DRAWING_PACE};
+
+pub(crate) async fn read_and_paint(app: &mut ApplicationContext<'_>, fpath: String) -> Result<()> {
+    let mut ring = AllocRingBuffer::new(37);
+
+    for ds in serde_jsonlines::json_lines(fpath)? {
+        let ds: DrawingBis = ds?;
+        let ds: Vec<Drawing> = ds.into_vec();
+
+        for d in ds {
+            let c = d.color();
+
+            info!(target:env!("CARGO_PKG_NAME"), "drawing XxY: {x}x{y}",
+                x = d.xs.len(),
+                y = d.ys.len(),
+            );
+
+            sleep(if true { DRAWING_PACE } else { INTER_DRAWING_PACE }).await;
+            paint(app, &d).await;
+
+            if c == drawing::Color::Black {
+                ring.enqueue(d.clone());
+            }
+            while ring.is_full() {
+                let Some(x) = ring.dequeue() else { break };
+                let x = Drawing { color: drawing::Color::White.into(), ..x };
+                paint(app, &x).await;
+            }
+        }
+    }
+
+    for x in ring.drain() {
+        let x = Drawing { color: drawing::Color::White.into(), ..x };
+        paint(app, &x).await;
+    }
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct DrawingBis {
+    #[allow(dead_code)]
+    pub word: String,
+
+    #[allow(dead_code)]
+    pub countrycode: String,
+
+    #[allow(dead_code)]
+    pub timestamp: String, // prolly chrono::datetime
+
+    pub recognized: bool,
+
+    #[allow(dead_code)]
+    pub key_id: String, // stringified bigint
+
+    pub drawing: Vec<[Vec<u8>; 2]>,
+}
+
+const PRESSURE: i32 = 2000;
+const WIDTH: u32 = 2;
+
+impl DrawingBis {
+    fn into_vec(self) -> Vec<Drawing> {
+        let DrawingBis { recognized, drawing, .. } = self;
+        if !recognized {
+            return vec![];
+        }
+        drawing
+            .into_iter()
+            .map(|[xs, ys]| {
+                let n = xs.len();
+                assert_eq!(n, ys.len());
+                Drawing {
+                    xs: xs.into_iter().map(f32::from).collect(),
+                    ys: ys.into_iter().map(f32::from).collect(),
+                    pressures: repeat(PRESSURE).take(n).collect(),
+                    widths: repeat(WIDTH).take(n).collect(),
+                    color: drawing::Color::Black as i32,
+                }
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn reads_a_drawing_from_jsonl() {
+    #[inline]
+    fn d() -> DrawingBis {
+        DrawingBis {
+            word: "tornado".into(),
+            countrycode: "US".into(),
+            timestamp: "2017-03-06 19:26:57.74406 UTC".into(),
+            recognized: true,
+            key_id: "6522189199179776".into(),
+            drawing: vec![
+                [
+                    vec![
+                        136, 114, 99, 54, 46, 65, 112, 150, 180, 180, 174, 151, 89, 6, 0, 6, 28,
+                        77, 179, 184, 184, 178,
+                    ],
+                    vec![
+                        0, 3, 8, 29, 36, 43, 51, 47, 32, 26, 19, 11, 15, 35, 38, 45, 56, 64, 70,
+                        64, 57, 28,
+                    ],
+                ],
+                [vec![1, 53, 84, 106], vec![39, 137, 183, 255]],
+                [vec![106, 148, 160, 176, 178], vec![255, 158, 98, 60, 45]],
+            ],
+        }
+    }
+
+    let p: Vec<Drawing> = d().into_vec();
+
+    assert_eq!(p.len(), 3);
+    for d in p {
+        let n = d.xs.len();
+        assert_ne!(n, 0);
+        assert_eq!(n, d.xs.len());
+        assert_eq!(n, d.ys.len());
+        assert_eq!(n, d.pressures.len());
+        assert_eq!(n, d.widths.len());
+        assert_eq!(drawing::Color::Black, d.color());
+    }
+}
---
+++ scrolls/src/paint.rs
@@ -0,0 +1,79 @@
+use std::time::Duration;
+
+use libremarkable::{
+    appctx::ApplicationContext,
+    framebuffer::{
+        cgmath,
+        cgmath::EuclideanSpace,
+        common::{color, display_temp, dither_mode, waveform_mode, DRAWING_QUANT_BIT_3},
+        FramebufferDraw, FramebufferRefresh, PartialRefreshMode,
+    },
+};
+use pb::proto::hypercards::{drawing, Drawing};
+use tokio::time::sleep;
+
+pub(crate) const DRAWING_PACE: Duration = Duration::from_millis(2);
+pub(crate) const INTER_DRAWING_PACE: Duration = Duration::from_millis(8);
+
+pub(crate) async fn paint(app: &mut ApplicationContext<'_>, drawing: &Drawing) {
+    let col = match drawing.color() {
+        drawing::Color::White => color::WHITE,
+        _ => color::BLACK,
+    };
+    assert!(drawing.xs.len() >= 3);
+    for i in 0..(drawing.xs.len() - 2) {
+        if i != 0 {
+            sleep(DRAWING_PACE).await;
+        }
+        let points: Vec<(cgmath::Point2<f32>, i32, u32)> = vec![
+            // start
+            (
+                cgmath::Point2 { x: drawing.xs[i], y: drawing.ys[i] },
+                drawing.pressures[i],
+                drawing.widths[i],
+            ),
+            // ctrl
+            (
+                cgmath::Point2 { x: drawing.xs[i + 1], y: drawing.ys[i + 1] },
+                drawing.pressures[i + 1],
+                drawing.widths[i + 1],
+            ),
+            // end
+            (
+                cgmath::Point2 { x: drawing.xs[i + 2], y: drawing.ys[i + 2] },
+                drawing.pressures[i + 2],
+                drawing.widths[i + 2],
+            ),
+        ];
+        let radii: Vec<f32> = points
+            .iter()
+            .map(|(_, pressure, tip)| (((*tip as f32) * (*pressure as f32)) / 2048.) / 2.)
+            .collect();
+        // calculate control points
+        let start_point = points[2].0.midpoint(points[1].0);
+        let ctrl_point = points[1].0;
+        let end_point = points[1].0.midpoint(points[0].0);
+        // calculate diameters
+        let start_width = radii[2] + radii[1];
+        let ctrl_width = radii[1] * 2.;
+        let end_width = radii[1] + radii[0];
+
+        let framebuffer = app.get_framebuffer_ref();
+        let rect = framebuffer.draw_dynamic_bezier(
+            (start_point, start_width),
+            (ctrl_point, ctrl_width),
+            (end_point, end_width),
+            10,
+            col,
+        );
+        framebuffer.partial_refresh(
+            &rect,
+            PartialRefreshMode::Async,
+            waveform_mode::WAVEFORM_MODE_DU,
+            display_temp::TEMP_USE_REMARKABLE_DRAW,
+            dither_mode::EPDC_FLAG_EXP1,
+            DRAWING_QUANT_BIT_3,
+            false,
+        );
+    }
+}
