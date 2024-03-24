e37fe122d9d375cb6993e22ef0a361479ec48836
Pierre Fenoll <pierrefenoll@gmail.com>
2024-03-24 18:07:10 +0100

    how
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pb/build.rs
+++ pb/build.rs
@@ -1,17 +1,21 @@
-use std::{env, path::PathBuf};
+use std::{env /*path::PathBuf*/};
 
-use anyhow::{bail, Result};
+// use anyhow::{bail, Result};
 
-fn main() -> Result<()> {
-    prepend_path("./bin")?; // so our `protoc` is found first
+// fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // prepend_path("./bin")?; // so our `protoc` is found first
+
+    env::set_var("PROTOC", "./protoc");
+
     tonic_build::compile_protos("proto/whiteboard.proto")?;
     Ok(())
 }
 
-fn prepend_path(dir: &str) -> Result<()> {
-    const PATH: &str = "PATH";
-    let Some(path) = env::var_os(PATH) else { bail!("No {PATH} env?") };
-    let paths = env::split_paths(&path).chain([PathBuf::from(dir)]);
-    env::set_var(PATH, env::join_paths(paths)?);
-    Ok(())
-}
+// fn prepend_path(dir: &str) -> Result<()> {
+//     const PATH: &str = "PATH";
+//     let Some(path) = env::var_os(PATH) else { bail!("No {PATH} env?") };
+//     let paths = [PathBuf::from(dir)].into_iter().chain(env::split_paths(&path));
+//     env::set_var(PATH, env::join_paths(paths)?);
+//     Ok(())
+// }
