f5da102f1b5c4b3cfe04e87c99fc2bc5378770ad
Pierre Fenoll <pierrefenoll@gmail.com>
2024-03-24 18:33:58 +0100

    that does it
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pb/build.rs
+++ pb/build.rs
@@ -1,21 +1,7 @@
-use std::{env /*path::PathBuf*/};
-
-// use anyhow::{bail, Result};
-
-// fn main() -> Result<()> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // prepend_path("./bin")?; // so our `protoc` is found first
-
-    env::set_var("PROTOC", "./protoc");
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    std::env::set_var("PROTOC", protoc);
 
     tonic_build::compile_protos("proto/whiteboard.proto")?;
     Ok(())
 }
-
-// fn prepend_path(dir: &str) -> Result<()> {
-//     const PATH: &str = "PATH";
-//     let Some(path) = env::var_os(PATH) else { bail!("No {PATH} env?") };
-//     let paths = [PathBuf::from(dir)].into_iter().chain(env::split_paths(&path));
-//     env::set_var(PATH, env::join_paths(paths)?);
-//     Ok(())
-// }
